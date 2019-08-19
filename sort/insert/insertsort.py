100b57696f48c9dceabc0cc88f2974e653e46639
hyeonsuk <qu3230@gmail.com>
2019-08-19 17:00:01 +0900

    insert 정렬

--- sort/insert/insertsort.py
+++ sort/insert/insertsort.py
@@ -2,8 +2,21 @@ import random
 
 def ascSort(arr):
 
-  for i in range(0, 98):
-    for j=i+1 in range(0, 99)
+  for i in range(0, 99):
+    indexMin = i
+    for j in range(i+1, 100):
+      if arr[indexMin]>arr[j]:
+        indexMin = j
+    arr[indexMin], arr[i] = arr[i], arr[indexMin]
+
+def desSort(arr):
+
+  for i in range(0, 99):
+    indexMax = i
+    for j in range(i+1, 100):
+      if arr[indexMax]<arr[j]:
+        indexMax = j
+    arr[indexMax], arr[i] = arr[i], arr[indexMax]
 
 
 if __name__ == "__main__":
