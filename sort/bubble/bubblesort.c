30ac72fbf7cf3fffc4dd2d8f4f547dfe37dc56a6
hyeonsuk <qu3230@gmail.com>
2019-12-02 23:06:48 +0900

    sort

--- sort/bubble/bubblesort.c
+++ sort/bubble/bubblesort.c
@@ -1,4 +1,3 @@
-// 매번 연속된 두 개 인덱스를 비교하여 (오름차순일 경우) 큰 값을 뒤로 넘겨 정렬
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,27 +9,27 @@ void swap(int *arr, int a, int b) {
     arr[b] = tmp;
 }
 
-void printArray(int arr[], int cnt)
-{
-  for(int i=0; i<cnt; i++) {
-
-    printf("%d ", arr[i]);
-  }
-  printf("\n");
+void printArray(int arr[], int cnt) {
+	int i;
+	for(i=0; i<cnt; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
 }
 
 int main()
 {
+  int i, j;
   int data[CNT] = {5, 15, 7, 20, 1, 4, 10, 2, 8, 11, 9, 6, 13, 18, 3, 16, 12, 14, 17, 19};
   int data2[CNT] = {5, 15, 7, 20, 1, 4, 10, 2, 8, 11, 9, 6, 13, 18, 3, 16, 12, 14, 17, 19};
   int temp = 0;
 
-  printf("Random Data: ");
+  printf("[1]Random Data: ");
   printArray(data, sizeof(data)/sizeof(int));
 
-  for(int i=0; i<CNT-1; i++) {
-    for(int j=0; j<CNT-1-i; j++) {
-      if(data[j]>data[j+1]) {
+  for(i=0; i<CNT-1; i++) {
+    for(j=0; j<CNT-1-i; j++) {
+      if(data[j] > data[j+1]) {
         temp = data[j];
         data[j] = data[j+1];
         data[j+1] = temp;
@@ -38,24 +37,24 @@ int main()
     }
   }
 
-  printf("Sorted Data: ");
+  printf("[1]Sorted Data: ");
   printArray(data, sizeof(data)/sizeof(int));
 
   printf("\n\n");
   temp = 0;
 
-  printf("Random Data: ");
+  printf("[2]Random Data: ");
   printArray(data2, sizeof(data2)/sizeof(int));
 
-  for(int i=0; i<CNT-1; i++) {
-    for(int j=0; j<CNT-1-i; j++) {
-      if(data2[j]<data2[j+1]) {
+  for(i=0; i < CNT; i++) {
+    for(j=0; j<CNT-1-i; j++) {
+      if(data2[j] > data2[j+1]) {
         swap(data2, j, j+1);
       }
     }
   }
 
-  printf("Sorted Data: ");
+  printf("[2]Sorted Data: ");
   printArray(data2, sizeof(data2)/sizeof(int));
 
   return 0;
--- sort/insert/insertsort.c
+++ sort/insert/insertsort.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 
 #define CNT 20
+void cleanBuf(void) {
+	while(getchar()!='\n');
+}
 
 void printArray(int arr[], int cnt)
 {
-  for(int i=0; i<cnt; i++) {
+  int i;
+  for(i=0; i<cnt; i++) {
 
     printf("%d ", arr[i]);
   }
   printf("\n");
+  
 }
 
 int main()
 {
+  int i, j;
   int data1[CNT] = {5, 15, 7, 20, 1, 4, 10, 2, 8, 11, 9, 6, 13, 18, 3, 16, 12, 14, 17, 19};
   int data2[CNT] = {5, 15, 7, 20, 1, 4, 10, 2, 8, 11, 9, 6, 13, 18, 3, 16, 12, 14, 17, 19};
+  int data3[CNT] = {5, 15, 7, 20, 1, 4, 10, 2, 8, 11, 9, 6, 13, 18, 3, 16, 12, 14, 17, 19};
   int temp = 0;
 
-  printf("Random Data: ");
+  printf("[1]Random Data: ");
   printArray(data1, sizeof(data1)/sizeof(int));
 
   int indexMin = 0;
-  for(int i=0; i<CNT-1; i++) {
+  for(i=0; i<CNT-1; i++) {
     indexMin = i;
-    for (int j=i+1; j<CNT; j++) {
+    for (j=i+1; j<CNT; j++) {
       if(data1[indexMin]>data1[j]) {
         indexMin = j;
       }
@@ -33,27 +40,43 @@ int main()
     data1[i] = temp;
   }
 
-  printf("Sorted Data: ");
+  printf("[1]Sorted Data: ");
   printArray(data1, sizeof(data1)/sizeof(int));
+  
+  printf("[3]Random Data: ");
+  printArray(data3, sizeof(data3)/sizeof(int));
 
-  printf("Random Data: ");
-  printArray(data2, sizeof(data2)/sizeof(int));
-
-  int indexMax = 0;
-  for(int i=0; i<CNT-1; i++) {
-    indexMax = i;
-    for (int j=i+1; j<CNT; j++) {
-      if(data2[indexMax]<data2[j]) {
-        indexMax = j;
-      }
-    }
-    temp = data2[indexMax];
-    data2[indexMax] = data2[i];
-    data2[i] = temp;
+  for(i = 0; i < CNT; i++) {
+  	for(j = i + 1; j > i; j--) {
+  		if(data3[j] < data3[j - 1]) {
+  			temp = data3[j - 1];
+  			data3[j - 1] = data3[j];
+  			data3[j] = temp;
+		}
+	}
   }
 
-  printf("Sorted Data: ");
+  printf("[3]Sorted Data: ");
+  printArray(data3, sizeof(data3)/sizeof(int));
+
+  printf("[2]Random Data: ");
   printArray(data2, sizeof(data2)/sizeof(int));
+
+  for(i = 0; i < CNT - 1; i++) {
+  	j = i;
+  	while(data2[j] > data2[j + 1]) {
+  		temp = data2[j];
+  		data2[j] = data2[j + 1];
+  		data2[j + 1] = temp;
+  		j--;
+	}
+  }
+
+  printf("[2]Sorted Data: ");
+  printArray(data2, sizeof(data2)/sizeof(int));
+  
+  
+  
 
   return 0;
 }
